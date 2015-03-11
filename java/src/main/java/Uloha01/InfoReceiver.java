9d34d456f5499be491f130f17ed2f645c05888a2
Patrik Pekarčik <patrik@recepcia-online.sk>
2015-03-11 12:43:41 +0100

    Uloha01 - InfoReceiver created

---
+++ java/src/main/java/Uloha01/InfoReceiver.java
@@ -0,0 +1,43 @@
+package Uloha01;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class InfoReceiver {
+    public static void main(String[] args) {
+        try {
+            DatagramSocket soket = new DatagramSocket(InfoSender.INFO_RECEIVER_PORT);
+            DatagramPacket paket = null;
+            while (true) {
+                byte[] buf = new byte[soket.getReceiveBufferSize()];
+                paket = new DatagramPacket(buf, buf.length);
+                soket.receive(paket);
+                buf = paket.getData();
+
+                ByteArrayInputStream bais = new ByteArrayInputStream(buf);
+                ObjectInputStream ois = new ObjectInputStream(bais);
+
+                long fileSize = ois.readLong();
+                String fileName = ois.readUTF();
+                int fileSenderPort = ois.readInt();
+                int fileReceiverPort = ois.readInt();
+
+                StringBuilder sb = new StringBuilder();
+                sb.append(paket.getAddress() + ":");
+                sb.append(fileName + " (" + fileSize + ") ");
+                sb.append(fileSenderPort + " ");
+                sb.append(fileReceiverPort);
+
+                System.out.println(sb.toString());
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
